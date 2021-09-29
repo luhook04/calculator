8896db6af2494fdfded847e6c97561cac5e6e83a
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2021-09-29 15:34:30 -0400

    added all the operator functions and the function to call desired operator function

---
+++ app.js
@@ -0,0 +1,18 @@
+const add = (a, b) => {
+  return a + b;
+};
+const subtract = (a, b) => {
+  return a - b;
+};
+const multiply = (a, b) => {
+  return a * b;
+};
+const divide = (a, b) => {
+  return a / b;
+};
+
+const operate = (func) => {
+  return func;
+};
+
+console.log(operate(multiply(10, 5)));
