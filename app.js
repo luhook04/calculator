2b19ad5963ccd5437ea2ccf08895596e74e8864a
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2021-09-29 20:34:29 -0400

    added a function to display the user specified number

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// declare references variables
+const display = document.querySelector('#display');
+
+// select all buttons with a number
+const numbers = document.querySelectorAll('.numbers');
+// basic math functions
 const add = (a, b) => {
   return a + b;
 };
@@ -11,8 +17,17 @@ const divide = (a, b) => {
   return a / b;
 };
 
+// takes a math function and it's arguments and returns the answer
 const operate = (func) => {
   return func;
 };
 
-console.log(operate(multiply(10, 5)));
+// updates display to text content of clicked button
+const updateDisplay = (e) => {
+  display.textContent += e.target.textContent;
+};
+
+// applys the updateDisplay function to each number on click
+numbers.forEach((number) => {
+  number.addEventListener('click', updateDisplay);
+});
