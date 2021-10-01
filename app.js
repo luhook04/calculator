0f3387fbc3b6be75c50528cb5adea58e41f26cd3
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2021-10-01 17:31:07 -0400

    added a switch statement to take in the different operators and put together the equal function:

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 // declare variables
 const display = document.querySelector('#display');
+const equal = document.querySelector('#equal');
 const clearBtn = document.querySelector('#clear');
 const multBtn = document.querySelector('#multiply');
 const addBtn = document.querySelector('#add');
 const subBtn = document.querySelector('#subtract');
 const divBtn = document.querySelector('#divide');
 
-let displayValue = '';
-let operand;
+let inputArray = [];
+let operatorArray = [];
+let numberArray = [];
+let userNum;
+
 // select all buttons with a number and operand
 const numbers = document.querySelectorAll('.numbers button');
-const operators = document.querySelectorAll('.operands button');
+const operators = document.querySelectorAll('.operators button');
 
 // basic math functions
 const add = (a, b) => {
@@ -26,41 +30,101 @@ const divide = (a, b) => {
   return a / b;
 };
 
-// takes a math function and it's arguments and returns the answer
-const operate = (operator, num1, num2) => {
-  return operator(num1, num2);
-};
+// function that takes all the math functions
+const operate = (operator, a, b) => {
+  switch (operator) {
+    case '+':
+      result = add(a, b);
+      break;
 
-// updates display to text content of clicked button
-const updateDisplay = (e) => {
-  if (display.textContent === '0') {
-    display.textContent = e.target.textContent;
+    case '*':
+      result = multiply(a, b);
+      break;
+
+    case '/':
+      result = divide(a, b);
+      break;
+
+    case '-':
+      result = subtract(a, b);
+      break;
+
+    default:
+      console.log('ERROR...');
   }
-  else {
-    display.textContent += e.target.textContent;
+  return result;
+};
+
+// updates display to equal the sequence of button hits before user hits an operator
+const updateDisplay = () => {
+  if (inputArray.length === 0) {
+    inputArray.push(0);
   }
+  display.textContent = inputArray.join('');
 };
 
-// added the clear function to update display to 0 when clear button is clicked
-const clearDisplay = () => {
-  display.textContent = '0';
-};
-
-clearBtn.addEventListener('click', clearDisplay);
-
-const getNumber = () => {
-  displayValue = parseFloat(display.textContent);
-};
-
-const getOperand = function(e) {
-  operand = e.target.id;
-  console.log(operand);
-};
-// applys the updateDisplay function to each number on click and operators
+// adds a function to each number click that puts it's value in the inputArray
+// and updates the display
 numbers.forEach((number) => {
-  number.addEventListener('click', updateDisplay);
+  number.addEventListener('click', function(e) {
+    if (inputArray[0] == 0) {
+      inputArray.pop();
+    }
+    inputArray.push(e.target.textContent);
+    updateDisplay();
+  });
 });
 
+// each time an operator is clicked it adds operator to an array
+// and joins all the separate numbers of the number array together in the next index
 operators.forEach((operator) => {
-  operator.addEventListener('click', getOperand);
+  operator.addEventListener('click', function(e) {
+    userNum = inputArray.join('');
+    if (userNum !== '') {
+      numberArray.push(userNum);
+    }
+    inputArray.length = 0;
+    operatorArray.push(e.target.textContent);
+  });
+});
+
+// sets display back to zero and resets all stored values
+clearBtn.addEventListener('click', function() {
+  inputArray = [];
+  numberArray = [];
+  operatorArray = [];
+  display.textContent = 0;
+});
+
+// iterates through the arrays and plugs them as args into the operate function
+equal.addEventListener('click', function() {
+  numberArray.push(inputArray.join(''));
+  if (
+    operatorArray.length > 0 &&
+    numberArray.length == operatorArray.length + 1
+  ) {
+    for (let i = 0; i < numberArray.length - 1; i++) {
+      numberArray[i + 1] = operate(
+        operatorArray[i],
+        parseFloat(numberArray[i]),
+        parseFloat(numberArray[i + 1])
+      );
+    }
+    let result = numberArray.pop();
+    if (result == Infinity || result == -Infinity) {
+      console.log('Infinite');
+    }
+    else {
+      display.textContent = result.toFixed(2);
+    }
+    inputArray.length = 0;
+    numberArray.length = 0;
+    operatorArray.length = 0;
+  }
+  else {
+    console.log('Invalid Input');
+    inputArray.length = 0;
+    numberArray.length = 0;
+    operatorArray.length = 0;
+  }
 });
